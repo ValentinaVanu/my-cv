6fef01a4b6d11cd26d27fc6e8d4cdd34a1659311
Valentina Vanu <vanuvalentina16@gmail.com>
2017-05-30 11:43:14 +0100

    Update image gulp process to support more image types

--- gulp/image.js
+++ gulp/image.js
@@ -2,13 +2,10 @@
 
 import path from 'path';
 import pngquant from 'imagemin-pngquant';
-import jpegoptim from 'imagemin-jpegoptim';
+import jpegtran from 'imagemin-jpegtran';
 import optipng from 'imagemin-optipng';
 import gifsicle from 'imagemin-gifsicle';
 import svgo from 'imagemin-svgo';
-import { getImageCollection } from './util/util';
-import mergeStream from 'merge-stream';
-import gulpConfig from './util/config';
 
 const image = ({
   gulp,
@@ -19,60 +16,33 @@ const image = ({
 }) => {
   const dir = config.directory;
   const dest = path.join(taskTarget, dir.asset.replace(/\_/, ''), dir.image);
-  const templateCollection = dir.templateCollection;
-  const templateImagePathCollection = templateCollection.map(folderName => {
-    return {
-      source: path.join(
-        dir.source,
-        `_${folderName}`,
-        gulpConfig.fileExpression.image
-      ),
-      dest: path.join(
-        taskTarget,
-        `${folderName}`
-      )
-    };
-  });
-
-  const assetImagePath = path.join(
-    dir.source,
-    dir.asset,
-    dir.image,
-    gulpConfig.fileExpression.image
-  );
 
   gulp.task('image', () => {
-    let gulpStreamCollection = templateImagePathCollection
-      .map(({source, dest}) => getImageCollection({
-        source,
-        gulp,
-        config,
-        plugins,
-        pngquant,
-        jpegoptim,
-        gifsicle,
-        svgo,
-        dest,
-        optipng,
-        args
-      })
-    );
-
-    let assetImageCollection = getImageCollection({
-      source: assetImagePath,
-      gulp,
-      config,
-      plugins,
-      pngquant,
-      jpegoptim,
-      gifsicle,
-      svgo,
-      dest,
-      optipng,
-      args
-    });
-
-    return mergeStream(gulpStreamCollection, assetImageCollection);
+    return gulp
+      .src(path.join(
+        dir.source,
+        dir.asset,
+        dir.image,
+        '**/*.{jpg,jpeg,gif,svg,png}'
+      ))
+      .pipe(plugins.changed(dest))
+      .pipe(plugins.if(
+        args.production,
+        plugins.imagemin({
+          progressive: true,
+          optimizationLevel: 7,
+          svgoPlugins: [{
+            removeViewBox: false
+          }],
+          use: [
+            pngquant({
+              speed: 10
+            }),
+            jpegtran(), optipng(), gifsicle()
+          ]
+        })
+      ))
+      .pipe(gulp.dest(dest));
   });
 };
 
